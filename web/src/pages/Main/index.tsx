f5d5f0772dd225f560cec1e22cadfcc40b168529
Hudson Duarte <hudsoneeto@outlook.com>
2020-08-08 15:19:14 -0300

    :fire:

--- web/src/pages/Main/index.tsx
+++ web/src/pages/Main/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './styles.css'
+import perfilImg from '../../assets/images/exemplo.png'
 
 
 function Main(){
@@ -7,7 +8,7 @@ function Main(){
         <div id="main">
             <div id="page-main" className="container">
                 <div id="landing-title">
-                    <h1 id="title">CANTOS DE ELITE</h1>
+                    <h1>CANTOS DE ELITE</h1>
                 </div>
                 <div id="bgi">
                     <div id="content">
@@ -22,9 +23,17 @@ function Main(){
                         </div>
                     </div>
                 </div>
+                <div id="perfil">
+                <h1 id="perfil-name">QUEM É CAIO PÊPE</h1>
+                <div id="textos">
+                    <p id="textoum">Meu nome é <strong>Caio Pêpe</strong>, tenho <strong>18 anos</strong> e sempre fui fascinado em fontes ativas de renda. No ano de 2020 consegui sair do absoluto zero a faturamentos de até <strong>cinco dígitos com o trade esportivo</strong>, sendo capaz de ajudar minha família e sair das asas dos meus pais fazendo <strong>a minha própria renda</strong>.</p>
+                    <img src={perfilImg} alt="Imagem de Perfil" id="perfil-img"/>
+                    <p id="textodois"><strong>Nessa nova jornada</strong> quero trazer essa mesma <strong>independência</strong> para vocês, e lhes mostrar que tudo pode ser alcançado através de <strong>persistência e consistência</strong> juntamente a um trabalho de médio a longo prazo e estratégias utilizadas pela <strong>elite dos apostadores</strong>. Vamos juntos que te mostro <strong>o lado vencedor</strong>! </p>    
+                 </div>
             </div>
+            </div>
+            
         </div>
-        
     )
 }
 
