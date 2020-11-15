3f1405e003e4d7608e6c0938655a9ba4759ce8e2
Hudson Duarte <hudsoneeto@outlook.com>
2020-11-15 20:29:37 -0300

    eslint + alteraçoes no rumo do projeto

--- web/src/pages/Main/index.tsx
+++ web/src/pages/Main/index.tsx
@@ -24,10 +24,10 @@ import img17 from '../../assets/images/prints/17.jpeg'
 import reembolso from '../../assets/images/reembolso.png'
 
 
-
-function Main(){
-    return (
-        <div id="main">
+const Main: React.FC = () => {
+  return (
+    <>
+      <div id="main">
             <div id="page-main">
                 <div id="cantosdeelite">
                     <div id="landing-title">
@@ -37,34 +37,33 @@ function Main(){
                         <div id="content">
                             <div id="text">
                                 <span>CONHEÇAM OS <strong>MÉTODOS</strong> QUE ME FIZERAM <strong>FATURAR</strong></span>
-                                <p id="title"><br/>5 DÍGITOS <strong>A PARTIR DO ZERO</strong>.</p> 
+                                <p id="title"><br/>5 DÍGITOS <strong>A PARTIR DO ZERO</strong>.</p>
                             </div>
                             <div id="video">
                                 <iframe width="560" height="315" src="https://www.youtube.com/embed/etZWWS8oEh0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
                             </div>
                             <div id="buttonbuy">
                                 <a href="">
-                                    <span>ADQUIRA O E-BOOK AGORA!</span>
+                                    <span>ADQUIRA O CURSO AGORA!</span>
                                 </a>
                             </div>
                             <div id="price">
-                                <p id="desconto">Últimas vagas com 40% de desconto</p>
-                                <p id="old-price">DE R$497</p>
+                                <p id="old-price">DE R$297</p>
                                 <p id="per">POR APENAS</p>
-                                <p id="new-price">R$297</p>
-                                <p id="info1">Acesso Imediato Por Apenas <br/> 12 pagamentos de <strong>R$24,75</strong></p>
-                                <p id="info2">Menos de <strong>R$1</strong> por dia <br/><br/><br/></p>
+                                <p id="new-price">R$150</p>
+                                <p id="info1">Acesso Imediato Por Apenas <br/> 12 pagamentos de <strong>R$12,50</strong></p>
+                                <p id="info2">Menos de <strong>30</strong> vagas disponíveis <br/><br/><br/></p>
                             </div>
                     </div>
                 </div>
                 </div>
-                
+
                 <div id="perfil">
                     <h1 id="perfil-name">QUEM É CAIO PÊPE</h1>
                     <div id="textos">
                         <p id="textoum">Meu nome é <strong>Caio Pêpe</strong>, tenho <strong>18 anos</strong> e sempre fui fascinado em fontes ativas de renda. No ano de 2020 consegui sair do absoluto zero a faturamentos de até <strong>cinco dígitos com o trade esportivo</strong>, sendo capaz de ajudar minha família e sair das asas dos meus pais fazendo <strong>a minha própria renda</strong>.</p>
                         <img src={perfilImg} alt="Imagem de Perfil" id="perfil-img"/>
-                        <p id="textodois"><strong>Nessa nova jornada</strong> quero trazer essa mesma <strong>independência</strong> para vocês, e lhes mostrar que tudo pode ser alcançado através de <strong>persistência e consistência</strong> juntamente a um trabalho de médio a longo prazo e estratégias utilizadas pela <strong>elite dos apostadores</strong>. Vamos juntos que te mostro <strong>o lado vencedor</strong>! </p>    
+                        <p id="textodois"><strong>Nessa nova jornada</strong> quero trazer essa mesma <strong>independência</strong> para vocês, e lhes mostrar que tudo pode ser alcançado através de <strong>persistência e consistência</strong> juntamente a um trabalho de médio a longo prazo e estratégias utilizadas pela <strong>elite dos apostadores</strong>. Vamos juntos que te mostro <strong>o lado vencedor</strong>! </p>
                     </div>
                 </div>
                 <div id="modules">
@@ -72,36 +71,23 @@ function Main(){
                         <div id="modules-title">
                         <h1 id="h1-modules">O QUE EU VOU TE ENSINAR</h1>
                         </div>
-                        
+
                         <div id="modulos">
                             <div id="mod1">
                                 <p>
                                     <div id="module-name">
                                         <strong>Módulo 1</strong>
-                                        <img src={bookImg} alt=""/>
                                     </div>
                                     <br/>
-                                    <br/> <strong id="mod1-title">Primeiro Passo/ Estratégias:</strong> 
+                                    <br/> <strong id="mod1-title">Criação De Conta:</strong>
                                     <br/>
-                                    <br/> 1 - Termos que você precisa saber nas operações;
-                                    <br/>
-                                    <br/>
-                                    <br/> 2 - Primeira Estratégia de Cantos (Teórica/Escrita);
+                                    <br/> 1 - Verificação De Conta;
                                     <br/>
                                     <br/>
-                                    <br/> 3 - Primeira Estratégia de Cantos (Prática/Vídeo);
+                                    <br/> 2 - Como Depositar;
                                     <br/>
                                     <br/>
-                                    <br/> 4 - Segunda Estratégia de Cantos (Teórica/Escrita);
-                                    <br/>
-                                    <br/>
-                                    <br/> 5 - Segunda Estratégia de Cantos (Prática/Vídeo);
-                                    <br/>
-                                    <br/>
-                                    <br/> 6 - Terceira Estratégia de Cantos (Teórica/Escrita);
-                                    <br/>
-                                    <br/>
-                                    <br/> 7 - Terceira Estratégia de Cantos (Prática/Vídeo)
+                                    <br/> 3 - Como Sacar.
                                     <br/>
                                     <br/>
                                 </p>
@@ -110,24 +96,42 @@ function Main(){
                                 <p>
                                     <div id="module-name">
                                         <strong>Módulo 2</strong>
-                                        <img src={bookImg} alt=""/>
                                     </div>
                                         <br/>
                                         <br/>  <strong id="mod2-title">Handcap/Mindset para crescimento no mercado:</strong>
                                         <br/>
-                                        <br/> 1 - Explicação de Handcap (Teórica/Prática);
+                                        <br/> 1 - Estratégia De Canto Limite;
                                         <br/>
                                         <br/>
-                                        <br/> 2 - Perfis de investidor + Gestão de Banca;
+                                        <br/> 2 - Estratégia Diamante;
                                         <br/>
                                         <br/>
-                                        <br/> 3 - Mindset.
+                                        <br/> 3 - Estratégia Under Cantos;
+                                        <br/>
+                                        <br/>
+                                        <br/> 4 - Estratégia Ambos Marcam;
+                                        <br/>
+                                        <br/>
+                                        <br/> 5 - Estratégia De Alavancagem
+                                        <br/>
+                                        <br/>
+                                </p>
+                            </div>
+                            <div id="mod2">
+                                <p>
+                                    <div id="module-name">
+                                        <strong>Módulo 3</strong>
+                                    </div>
+                                        <br/>
+                                        <br/>  <strong id="mod2-title">Gestão de banca:</strong>
+                                        <br/>
+                                        <br/> 1 - Gestão De Banca
                                         <br/>
                                         <br/>
                                 </p>
                             </div>
                             <p><br/><br/></p>
-                        </div>       
+                        </div>
                     </div>
                 </div>
                 <div id="results">
@@ -138,7 +142,7 @@ function Main(){
                         <div id="prints">
                             <img src={img0} alt=""/>
                             <img src={img17} alt=""/>
-                            <img src={img5} alt=""/>                            
+                            <img src={img5} alt=""/>
                             <img src={img3} alt=""/>
                             <img src={img7} alt=""/>
                             <img src={img4} alt=""/>
@@ -164,7 +168,7 @@ function Main(){
                     <div id="buynow">
                         <div id="title">
                             <h1>GARANTA AGORA ACESSO </h1>
-                            <h1>AO MEU E-BOOK</h1>
+                            <h1>AO MEU CURSO</h1>
                             <h3>Não perca essa oportunidade</h3>
                             <h3>Aproveite uma condição especial</h3>
                             <h3>por tempo limitado:</h3>
@@ -189,9 +193,9 @@ function Main(){
                     <img src={reembolso} alt=""/>
                     <p>Por não ter histórico de reembolso, se em 7 dias você não gostar do conteúdo, <br/>ou por algum motivo achar que não faz sentido para você, <br/>eu devolvo 100% do seu dinheiro de volta. <br/>Basta nos enviar um e-mail solicitando reembolso.</p>
                 </div>
-            </div> 
+            </div>
         </div>
-    )
+    </>
+  );
 }
-
 export default Main;
